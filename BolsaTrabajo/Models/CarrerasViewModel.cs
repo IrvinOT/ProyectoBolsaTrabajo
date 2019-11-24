a3becc0a6be93fd023a70e1d687b186c54073446
Irvin Trejo <irvintrejoc@gmail.com>
2019-11-24 15:21:24 -0700

    Cambio version bottsrap, Modelo Carreras, index estaran las publicaciones, barra de busqueda por carrera

---
+++ BolsaTrabajo/Models/CarrerasViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BolsaTrabajo.Models
+{
+    public class CarrerasViewModel
+    {
+
+        public string Carrera { get; set; }
+
+        public IEnumerable<SelectListItem> ListaCarreras { get {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem {Text = "Sistemas Computacionales", Value="Sistemas Computacionales" },
+                    new SelectListItem {Text = "Mecatronica", Value="Mecatronica" },
+                    new SelectListItem {Text = "Logistica", Value="Logistica" },
+                    new SelectListItem {Text = "Industrial", Value="Industrial" },
+                    new SelectListItem {Text = "Gestion Empresarial", Value="Gestion Empresarial" },
+
+                };
+            }
+        }
+    }
+}
