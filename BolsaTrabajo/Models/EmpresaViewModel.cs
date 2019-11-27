f370f4c3ffdc782d36e72c47723a20c1ff84f880
IrvinOT <57695792+IrvinOT@users.noreply.github.com>
2019-11-26 19:01:36 -0700

    Modelo empresa

---
+++ BolsaTrabajo/Models/EmpresaViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BolsaTrabajo.Models
+{
+    public class EmpresaViewModel
+    {
+
+        public string NombreEmp { get; set; }
+        public string RazonEmp { get; set; }
+        public string GiroEmp { get; set; }
+        public string Calle { get; set; }
+        public string Colonia { get; set; }
+        public string Cp { get; set; }
+        public string Ciudad { get; set; }
+        public string TelefonoD { get; set; }
+        public string NombreE { get; set; }
+        public string DependenciaE { get; set; }
+        public string PuestoE { get; set; }
+        public string CorreoE { get; set; }
+        public string PassE { get; set; }
+        public string PassEC { get; set; }
+        public string TelefonoE { get; set; }
+    }
+}
