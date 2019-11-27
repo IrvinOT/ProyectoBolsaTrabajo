242327695a2e03ab4348a61ac1a7d46b0e4b9bd9
IrvinOT <57695792+IrvinOT@users.noreply.github.com>
2019-11-27 00:11:59 -0700

    Clases de conexion de prueba

---
+++ Biblioteca/Operacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Biblioteca
+{
+    class Operacion
+    {
+        public bool Insertar(string query)
+        {
+            Conexion cn = new Conexion();
+            try
+            {
+                SqlCommand cmd = new SqlCommand(query,cn.getConexion());
+                int n = cmd.ExecuteNonQuery();
+                return n > 0;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
